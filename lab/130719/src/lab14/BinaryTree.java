c99c4d284ae6acf48e7fb3f938ae6d96f29f4b98
Andrew Goh <mail.yisheng@gmail.com>
2013-07-22 00:21:23 -0700

    Got fibonacci tree to work, as well as the print function up

--- lab/130719/src/lab14/BinaryTree.java
+++ lab/130719/src/lab14/BinaryTree.java
@@ -1,5 +1,9 @@
 package lab14;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BinaryTree {
 
@@ -19,6 +23,24 @@ public class BinaryTree {
 	public void printPreorder ( ) {
 		printPreorder (myRoot);
 		System.out.println ( );
+	}
+	public static BinaryTree fibTree (int n) {
+	    BinaryTree result = new BinaryTree ( );
+	    result.myRoot = result.fibTreeHelper (n);
+	    return result;
+	}
+
+	private TreeNode fibTreeHelper (int n) {
+		if (n == 0) {
+			return new TreeNode(new Integer(0));
+		}
+		if (n == 1) {
+			return new TreeNode(new Integer(1));
+		}
+		Integer current1 = (Integer) fibTreeHelper(n-1).myItem;
+		Integer current2 = (Integer) fibTreeHelper(n-2).myItem;
+
+		return new TreeNode(current1.intValue() + current2.intValue(), fibTreeHelper(n-1), fibTreeHelper(n-2));
 	}
 
 	private static void printPreorder (TreeNode t) {
@@ -71,6 +93,9 @@ public class BinaryTree {
 		print (t, "sample tree 1");
 		t.fillSampleTree2 ( );
 		print (t, "sample tree 2");
+		t.check();
+		print (t.fibTree(5), "fibtree");
+		t.fibTree(5).print();
 	}
 
 	private static void print (BinaryTree t, String description) {
@@ -81,20 +106,51 @@ public class BinaryTree {
 		System.out.println ( );
 	}
 	
+	public void print() {
+		if (myRoot != null) {
+			printHelper(myRoot, 0);
+		}
+	}
+	
+	private static final String indent1 = "    ";
+	
+	private static void printHelper(TreeNode root, int indent) {
+		if (root != null) printHelper(root.myRight, indent + 1); else return;
+		println(root.myItem, indent);
+		if (root != null) printHelper(root.myLeft, indent + 1);
+	}
+	
+	private static void println(Object obj, int indent) {
+		for (int k = 0; k < indent; k++) {
+			System.out.print(indent1);
+		}
+		System.out.println(obj);
+	}
+	
+	
+	
 	private ArrayList alreadySeen;
 	
-	public boolean isOK() {
+	public boolean check() {
 		alreadySeen = new ArrayList();
 		try {
-			check(myRoot);
+			isOK(myRoot);
 			return true;
 		} catch (IllegalStateException e) {
 			return false;
 		}
 	}
 	
-	private void check(TreeNode t) throws IllegalStateException {
-		
+	private void isOK(TreeNode t) throws IllegalStateException {
+		Iterator<TreeNode> it = new TreeIterator();
+		while (it.hasNext()) {
+			TreeNode current = it.next();
+			if (alreadySeen.contains(current)) {
+				throw new IllegalStateException("It has already been seen!");
+			} else {
+				alreadySeen.add(current);
+			}
+		}
 	}
 	
 
@@ -114,5 +170,44 @@ public class BinaryTree {
 			myLeft = left;
 			myRight = right;
 		}
+
+	}
+		
+		
+	public class TreeIterator implements Iterator<TreeNode> {
+
+		public TreeIterator() {
+			if (myRoot != null) {
+				fringe.add(myRoot);
+			}
+		}
+		
+		private Queue fringe = new LinkedList();
+		
+		public boolean hasNext() {
+			return !fringe.isEmpty();
+		}
+	
+		public TreeNode next() {
+			if (!hasNext()) {
+				throw new NoSuchElementException("No more elements in the tree.");
+			}
+			TreeNode node = (TreeNode) fringe.remove();
+			
+			if (node.myLeft != null) {
+				fringe.add(node.myLeft);
+			}
+			
+			if (node.myRight != null) {
+				fringe.add(node.myRight);
+			}
+	
+			return node;
+		}
+		
+		public void remove() {
+			// not used
+		}
+
 	}
 }
